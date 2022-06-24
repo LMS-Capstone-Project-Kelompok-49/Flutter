7291ce13dd07398271ecc511aeb3ebc88b91bdf3
RadenAnji <ramzi.cimumut20@gmail.com>
2022-06-24 14:26:52 +0800

    navbar init

---
+++ lib/bottom_navigation.dart
@@ -0,0 +1,54 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:learnitnow/screen/assignment.dart';
+import 'package:learnitnow/screen/chat.dart';
+import 'package:learnitnow/screen/home.dart';
+import 'package:learnitnow/screen/profile.dart';
+
+class BottomNavigation extends StatefulWidget {
+  const BottomNavigation({Key? key}) : super(key: key);
+
+  @override
+  State<BottomNavigation> createState() => _BottomNavigationState();
+}
+
+class _BottomNavigationState extends State<BottomNavigation> {
+  int index = 0;
+
+  final screens = [
+    const Home(),
+    const Assignment(),
+    const Chat(),
+    const Profile()
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    final items = <Widget>[
+      const Icon(Icons.home, size: 30),
+      const Icon(Icons.assessment, size: 30),
+      const Icon(Icons.chat, size: 30),
+      const Icon(Icons.person, size: 30),
+    ];
+    return Scaffold(
+      extendBody: true,
+      backgroundColor: Colors.black,
+      body: screens[index],
+      bottomNavigationBar: Theme(
+        data: Theme.of(context)
+            .copyWith(iconTheme: const IconThemeData(color: Colors.white)),
+        child: CurvedNavigationBar(
+          color: const Color(0XFF00AFB9),
+          buttonBackgroundColor: const Color(0XFF006785),
+          backgroundColor: Colors.transparent,
+          height: 50,
+          animationCurve: Curves.easeInOut,
+          animationDuration: const Duration(milliseconds: 300),
+          index: index,
+          items: items,
+          onTap: (index) => setState(() => this.index = index),
+        ),
+      ),
+    );
+  }
+}
