e8d47938bb7c67bbb9a329cbd0b50a2f0cb8e4c3
RadenAnji <ramzi.cimumut20@gmail.com>
2022-07-01 19:30:09 +0800

    updated models (not fix)

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,43 +1,39 @@
 class User {
-  int? uid;
-  String? fullname;
+  int? id;
   String? email;
   String? password;
-  DateTime? dob;
   int? role;
+  int? profile;
   DateTime? created;
   DateTime? updated;
 
   User({
-    this.uid,
+    this.id,
     this.email,
-    this.fullname,
     this.password,
-    this.dob,
     this.role,
+    this.profile,
     this.created,
     this.updated,
   });
 
   User.fromJson(Map<String, dynamic> json){
-    uid = json['id'] as int;
+    id = json['id'] as int;
     email = json['email'] as String;
-    fullname = json['fullname'] as String;
     password = json['password'] as String;
-    dob = json['dob'] as DateTime;
     role = json['role'] as int;
+    profile = json['profile'] as int;
     created = json['created'] as DateTime;
     updated = json['updated'] as DateTime;
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['id'] = this.uid;
+    data['id'] = this.id;
     data['email'] = this.email;
-    data['fullname'] = this.fullname;
     data['password'] = this.password;
-    data['dob'] = this.dob;
     data['role'] = this.role;
+    data['profile'] = this.profile;
     data['created'] = this.created;
     data['updated'] = this.updated;
     return data;
