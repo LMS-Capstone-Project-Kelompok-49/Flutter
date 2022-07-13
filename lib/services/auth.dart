202581808e7713edaf68c7fb46cc39ea9ea00c45
RadenAnji <ramzi.cimumut20@gmail.com>
2022-07-13 17:34:23 +0800

    error

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,16 +1,14 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
-import 'package:learnitnow/models/user_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+// import 'package:learnitnow/models/user_model.dart';
 
 class AuthController {
   TextEditingController usernameController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
   Future loginUser() async {
-    const url = 'http://3.95.181.246/auth/login';
-
-    var response = await Dio().post(
+    Response response = await Dio().post(
       'http://3.95.181.246/auth/login',
       data: jsonEncode(
         {
@@ -21,14 +19,15 @@ class AuthController {
     );
     if (response.statusCode == 200) {
       var loginArr = json.decode(response.data);
-      // save this token in shared prefrences and make user logged in and navigate
-
+      // save token di shared preference
+      
       print(loginArr['token']);
       // return user;
     } else {
 
     }
   }
+
 
   Future<void> logOut() async {}
 }
